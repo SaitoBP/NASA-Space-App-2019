065d2db1ba7b119f91796669785149c6376f6635
Gabriel Saito <gabrielpereira050201@hotmail.com>
2019-10-19 15:57:21 -0300

    Finalizado a estrutura

---
+++ script/index.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  prevNextButtons: false,
+  pageDots: false,
+  draggable: true
+});
+
