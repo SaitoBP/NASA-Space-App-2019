8a20e60312b7915e89305eaa5f831b3aa02b23a7
Gabriel Saito <gabrielpereira050201@hotmail.com>
2019-10-19 22:40:58 -0300

    melhora da tela de planetas

--- script/index.js
+++ script/index.js
@@ -6,3 +6,53 @@ var flkty = new Flickity( elem, {
   pageDots: false,
   draggable: true
 });
+
+
+let size = "";
+
+document.getElementById("small").addEventListener("click",function(){
+  alert("Planeta pequeno");
+});
+console.log(document.getElementById("small"));
+
+
+document.getElementById("medium").addEventListener("click",  function(){
+  // alert("Planeta médio");
+  size = "medium"
+  flkty.next();
+})
+
+document.getElementById("large").addEventListener("click", function(){
+  size = "large";
+
+  let content = document.createElement("div");
+  content.className = "content";
+
+  let title = document.createElement("h1");
+  title.innerHTML = "O planeta é gasoso?";
+
+  let form_Y = document.createElement("input");
+  form_Y.setAttribute("type", "radio");
+  let label_Y = document.createElement("h3");
+  label_Y.innerHTML = "Sim";
+
+  let form_N = document.createElement("input");
+  form_N.setAttribute("type", "radio");
+  let label_N = document.createElement("h3");
+  label_N.innerHTML = "Não"
+  
+  content.appendChild(title);
+  content.appendChild(form_Y);
+  content.appendChild(label_Y);
+  content.appendChild(form_N);
+  content.appendChild(label_N);
+  
+  let cell = document.createElement("div");
+  cell.className = "carousel-cell";
+
+  cell.appendChild(content);
+
+  flkty.insert(cell, 4);
+
+  flkty.next();
+})
