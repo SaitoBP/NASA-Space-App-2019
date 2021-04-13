5ac26030210b7bccab631d38eba066271bb20098
Gabriel <gabrielpereira050201@hotmail.com>
2021-04-13 17:27:33 -0300

    Added express.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const PORT = 5000
+
+express()
+  .use(express.static('public'))
+  .get('/', (req, res) => res.sendFile('public/index.html'))
+  .listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`))
